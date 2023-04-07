68a79cc6fedbc18ea2cdfba4c973a5c3ec21b9b7
Olsi Odobashi <olsiodobashi@users.noreply.github.com>
2023-04-07 09:04:11 -0400

    Create route.ts

---
+++ src/Navigation/route.ts
@@ -0,0 +1,8 @@
+export enum Route {
+  Home          = '/',
+  Signin        = '/signin',
+  Register      = '/register',
+  Profile       = '/me',
+  UserSettings  = '/me/settings',
+  NotFound      = '/404'
+}
