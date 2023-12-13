7a1c3c48b0ab87b630369bb8b4553b0e6cd8a9b8
Olsi Odobashi <olsiodobashi@users.noreply.github.com>
2023-12-13 17:08:42 -0500

    Create job-type.enum.ts

---
+++ src/job-type.enum.ts
@@ -0,0 +1,8 @@
+export enum JobType {
+  CONTRACTOR = 'contractor',
+  FREELANCE = 'freelance',
+  FULL_TIME = 'full-time',
+  INTERNSHIP = 'internship',
+  PART_TIME = 'part-time',
+  TEMPORARY = 'temporary'
+}
